7ed9d1aa062f0d2ae679cce19fbec894033b9b14
Eshan Ashwani Gupta <eshangupta.prof@gmail.com>
2026-05-09 15:15:41 +0530

    changes made

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "https://ai-mock-interviewer-modern.onrender.com",
+    baseURL: "https://ai-mock-interviewer-modern.onrender.com"
 })
 
 export default API
